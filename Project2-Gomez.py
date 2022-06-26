31bdf4057dc1c33d89fe2e3414c0c57b2a7bb563
cgomezjr <108238951+cgomezjr@users.noreply.github.com>
2022-06-26 15:53:43 -0500

    Add files via upload

---
+++ Project2-Gomez.py
@@ -0,0 +1,1 @@
+print("Carlos Gomez\n417 Howard Dr\nDel City, OK 73115\n405-924-7932")
---
+++ Project5-Gomez.py
@@ -0,0 +1,4 @@
+base = int(input("Enter with base: "))
+height = int(input("Enter with height: "))
+area = .5 * base * height
+print("The are is", area, "square units.")
